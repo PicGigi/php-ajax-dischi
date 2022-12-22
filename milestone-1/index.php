042a81f1d37cdb5f40f5df018b2ca60e725ec9d2
PicGigi <junior.1997@live.it>
2022-12-22 16:37:53 +0100

    Primo commit

---
+++ milestone-1/index.php
@@ -0,0 +1,20 @@
+<?php include __DIR__ . '/db/data.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/style.css">
+	<title>Dischi</title>
+</head>
+<body>
+	<header>
+		<img src="img/spotify-logo.png" alt="">
+	</header>
+	<main>
+		
+	</main>
+</body>
+</html>
